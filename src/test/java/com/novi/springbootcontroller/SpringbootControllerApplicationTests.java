25c922df631f04cf2a540b23ec61535dea6724d6
BadKarmaL33t <jchagesteijn@protonmail.com>
2023-08-28 15:05:07 +0200

    structuur toevoegen

---
+++ src/test/java/com/novi/springbootcontroller/SpringbootControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novi.springbootcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
