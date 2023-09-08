58f12ae09ef354a745b1b571fb64a51191c74bf5
BadKarmaL33t <jchagesteijn@protonmail.com>
2023-09-08 13:03:03 +0200

    opdracht domain models inrichten door een tv class te maken met attributen, constructors, getters en setters. Daarnaast de mappenstructuur opzetten en settings configureren

---
+++ src/main/java/com/novi/springbootcontroller/repositories/TelevisionRepository.java
@@ -0,0 +1,8 @@
+package com.novi.springbootcontroller.repositories;
+
+import com.novi.springbootcontroller.models.Television;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TelevisionRepository extends JpaRepository<Television, Long> {
+
+}
