c1dc7651be172baaa345d7fe40d7da63c3dbaf1a
BadKarmaL33t <jchagesteijn@protonmail.com>
2023-09-12 19:41:56 +0200

    repositories aanmaken voor alle nieuwe models

---
+++ src/main/java/com/novi/springboottechiteasy/repositories/RemoteControllerRepository.java
@@ -0,0 +1,8 @@
+package com.novi.springboottechiteasy.repositories;
+
+import com.novi.springboottechiteasy.models.RemoteController;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RemoteControllerRepository extends JpaRepository<Long, RemoteController> {
+
+}
